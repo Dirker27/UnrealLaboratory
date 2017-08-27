ffef9841a80c1e0a3bb791a4a64db5d26fca2089
Dirk <hortensd@gmail.com>
2017-08-27 14:13:30 -0700

    [RoomEscape] Custom pawn and game mode blueprints

---
+++ Udemy_RoomEscape/Source/Udemy_RoomEscape/Grabber.h
@@ -0,0 +1,33 @@
+// Copyright ToasterCat Productions LLC - Please don't steal our shit.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UDEMY_ROOMESCAPE_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float GripStrength = 1.0f;
+
+private:
+	AActor* GrabbedObject;
+};
